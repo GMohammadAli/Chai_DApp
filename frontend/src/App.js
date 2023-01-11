3ef958cd92eb7ca6417ad174e9ca6c3724b270b4
GMohammadAli <gulammohammadali@gmail.com>
2023-01-11 21:16:06 +0530

    Error Resolved [Hardhat.config.js]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {useState, useEffect} from "react"
 import abi from './contract/chai.json';
+import { ethers } from 'hardhat';
 
 function App() {
   const [state,setState] = useState({
@@ -8,13 +9,31 @@ function App() {
     signer:null,
     contract: null
   });
-
+ 
+  const [account, setAccount] = useState("None")
   useEffect(() => {
-    const contractWallet = async () => {
-      const contractAddress = "";
-    }
-  })
+    const connectWallet = async () => {
+      const contractAddress = " 0x92b20883adcd8e60243ea5d61ce78caf82be7254";
+      const contractAbi = abi.abi;
+      try{
+        const {ethereum} = window;
 
+        if(ethereum){
+          const account = await ethereum.request({method: "eth_requestSccounts",})
+        }
+        const provider = new ethereum.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.ContractFactory(contractAddress,contractAbi,signer)
+        setState({provider,signer,contract})
+      }catch(err){
+        console.log(err)
+      }
+    }
+
+    connectWallet()
+  },[])
+
+  console.log(state)
   return (
     <div className="App">
       
